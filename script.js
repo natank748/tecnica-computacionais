27daab3572948422064b913903fb524fd1de35c1
natank748 <natan.goncalves.rosa@escola.pr.gov.br>
2026-09-14 11:16:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,11 +10,17 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Isso é assustador!",
-                afirmacao: "afirmacao"
+                afirmacao: [
+                    "No início ficou com medo do que essa tecnologia pode fazer.",
+                    ""
+            ]
             },
             {
                 texto: "Isso é maravilhoso!",
-                afirmacao: "afirmacao"
+                afirmacao: [ 
+                    "Quis saber como usar IA no seu dia a dia.",
+                    ""
+            ]
             }           
             
         ]
